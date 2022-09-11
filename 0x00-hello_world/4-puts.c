2f1a77178cc31ec3fb5ff6566d3be20b55b51a5a
TebohoMotsumi <TMotsumi@africanbank.co.za>
2022-09-11 13:42:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * * main - To demonstrate the puts keyword
+ *
+ *  * Return: return 0
+ *
+ *   */
+
+int main(void)
+
+{
+
+		char a[] = "\"Programming is like building a multilingual puzzle";
+
+
+
+			puts(a);
+
+
+
+				return (0);
+
+}
