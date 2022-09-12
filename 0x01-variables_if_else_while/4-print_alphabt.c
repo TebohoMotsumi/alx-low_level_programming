6a7eacf8da4833262c16d83d8b0cd1f1a3150561
TebohoMotsumi <TMotsumi@africanbank.co.za>
2022-09-12 13:37:28 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A programme that omits some alphabets
+ * Return: returns (0)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z');
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
