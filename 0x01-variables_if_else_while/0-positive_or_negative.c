725590a5986a1c98717d9e3839ebc5a40cf34a38
TebohoMotsumi <TMotsumi@africanbank.co.za>
2022-09-11 21:25:32 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Postive, negative or zero program
+ *
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+
+}
