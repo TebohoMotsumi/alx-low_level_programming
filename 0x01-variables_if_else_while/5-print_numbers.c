bcb9413685a68dad3f19f794271591325e7dc682
TebohoMotsumi <TMotsumi@africanbank.co.za>
2022-09-12 14:39:31 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints digits 0 - 9
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
