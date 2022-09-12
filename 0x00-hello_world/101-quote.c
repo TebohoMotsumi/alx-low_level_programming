2a6cfe97f418786026c9cc3ea10a958e5bcf7711
TebohoMotsumi <TMotsumi@africanbank.co.za>
2022-09-11 20:56:09 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - A program uses that prints to standard error
+ *
+ * Return: return 1
+ *
+ */
+
+
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - \
+			Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
